a69a1384a68cead75d81b9e8418eb2c7bc05ecf9
JaredOwen14 <jaredowen14@gmail.com>
2021-05-23 21:56:12 -0700

    week 4

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,11 @@ $(function() {
         }
     });
 });
+$(function() {
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+});
